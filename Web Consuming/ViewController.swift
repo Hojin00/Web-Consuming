25d40d8544fec51ffb91dea788b272c33f979da7
Hojin <khojin.ryu01@gmail.com>
2021-07-01 16:21:54 -0300

    movies upadate

--- Web Consuming/ViewController.swift
+++ Web Consuming/ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=2f8e29176bab443251fb4a3303db7498&language=en-US&page=1")!
+        
+        typealias WebMovies = [String: Any]
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data,
+                  let json = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any],
+                  let charactersDictionary = json as? [WebMovies]
+            else {
+                
+                return
+            }
+            print(charactersDictionary)
+            
+        }
+        .resume()
     }
 
 
